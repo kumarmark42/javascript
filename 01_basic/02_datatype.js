c43dfd874ee070a1231348a24ceb6257029a1047
kumarmark42 <113471792+kumarmark42@users.noreply.github.com>
2023-10-05 13:51:23 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"  // use new js version
+
+// alert("hello")  // we are using nodejs not browser
+
+console.log("Manish")
+
+let name = "Manish"
+let age = 23
+let isloggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Manish");
+console.log(typeof 23);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
