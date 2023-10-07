bff3ef14c5f889db48570ae740d189a5818bb303
kumarmark42 <113471792+kumarmark42@users.noreply.github.com>
2023-10-07 07:34:31 +0000

    datatpe_summary

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -44,6 +44,42 @@ console.log(strpoint);
 console.log(typeof strpoint);
 
 
+// ****************************************Operation*******************************************************
 
 
+let value = 3;
+let negvalue = -value
 
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello "
+let str2 = "Manish"
+ console.log(str1+str2);
+
+ console.log(1 + "2");
+ console.log("1" + 2);
+ console.log("1" + 2 + 2);
+ console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 3
+
+console.log(num1);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+let mark = 100
+--mark
+console.log(mark);
