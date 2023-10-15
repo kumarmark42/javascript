869f26d55fa54f0c93ad10bf336fb65c7bb0f41e
kumarmark42 <113471792+kumarmark42@users.noreply.github.com>
2023-10-15 20:53:41 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+
+
+let i = 0;
+while (i <= 20) {
+    // console.log(`value of i is : ${i}`);
+    i = i + 2
+}
+
+
+
+myArray = ["hulk", "ironman", "spiderman", "flash"]
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`hero is ${myArray[arr]}`);
+    arr++
+}
+
+let score = 0
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
